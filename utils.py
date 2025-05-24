04ed27baee0217fc90b112e1a8eb75b9678e79d3
Dev-Nan <70539964+Dev-Nan@users.noreply.github.com>
2025-05-24 20:57:12 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,92 @@
+import numpy as np
+from config import config
+
+
+class Layer_Dense:
+
+    def __init__(self, n_inputs, n_neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
+
+        # Initialize weights and biases
+        self.weights = config.weight_initiase_factor* np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+        # Set regularization strength
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
+
+
+    def forward(self, inputs):
+
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+    def backward(self, dvalues):
+
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # L1 on weights
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+
+        # L2 on weights
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+
+        # L1 on biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+
+        # L2 on biases
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
+
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+class Activation_ReLU:
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
+
+
+class Activation_Softmax:
+    
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1,keepdims=True)
+        self.output = probabilities
+
+
+
+class Layer_Dropout:
+
+    def __init__(self, rate):
+        # Store the dropout rate, invert it to get the success rate
+        self.rate = 1 - rate
+
+
+    def forward(self, inputs):
+
+        self.inputs = inputs
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) / self.rate
+        self.output = inputs * self.binary_mask
+
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
+
+
